6f77fd9997a40cee6074b15fdc65dfda7d8d28ec
Ubuntu <azureuser@docker.iixjinv1xiwupgbar5pw3wtntf.fx.internal.cloudapp.net>
2020-05-15 20:04:30 +0000

    change extension vm settings

--- vm.tf
+++ vm.tf
@@ -93,7 +93,8 @@ resource "azurerm_virtual_machine_extension" "docker" {
 
   settings = <<SETTINGS
     {
-        "commandToExecute": "apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' && apt-get update && apt-get install docker-ce && systemctl enable docker && usermod -aG docker azureuser"
+        "fileUris": "https://raw.githubusercontent.com/santy8a/vm-blog/master/install-docker-ubuntu.sh",
+        "commandToExecute": "sh install-docker-ubuntu.sh"
     }
 SETTINGS
 }
