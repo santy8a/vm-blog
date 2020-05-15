255dd25edb9550cc7da103f6d74aa65a40f43cc5
Ubuntu <azureuser@docker.iixjinv1xiwupgbar5pw3wtntf.fx.internal.cloudapp.net>
2020-05-15 20:44:04 +0000

    Add domain label to ip address

--- vm.tf
+++ vm.tf
@@ -31,6 +31,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
     location            = azurerm_resource_group.main.location
     resource_group_name = azurerm_resource_group.main.name
     allocation_method   = "Dynamic"
+    domain_name_label   = "santy8a8a"
 }
 
 resource "azurerm_network_interface" "main" {
