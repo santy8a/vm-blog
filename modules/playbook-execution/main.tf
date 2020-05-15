d994c93b1c304b5168c9b31666fd1875906211ef
Ubuntu <azureuser@docker.iixjinv1xiwupgbar5pw3wtntf.fx.internal.cloudapp.net>
2020-05-15 22:48:08 +0000

    test

--- modules/playbook-execution/main.tf
+++ modules/playbook-execution/main.tf
@@ -1,12 +1,12 @@
 # This is how we call Ansible and pass in variables from Terraform.
-resource null_resource "install_docker_test" {
+resource null_resource "install_docker" {
   provisioner "local-exec" {
     command = <<EOT
     OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES \
     ANSIBLE_HOST_KEY_CHECKING="False" \
     ansible-playbook --inventory '${var.dns_label}', \
     -c paramiko -u '${var.username}' \
-    --extra-vars="{ \"ansible_password\": \"${var.password}\"}" ${var.ansible_playbook_path}
+    --extra-vars="{ \"ansible_password\": \"${var.password}\"}" ${var.ansible_playbook_path} -vvv
      EOT
   }
 }
