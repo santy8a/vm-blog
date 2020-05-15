ce08d2449f088aaa9a625028093d3eef481bc4ae
Ubuntu <azureuser@docker.iixjinv1xiwupgbar5pw3wtntf.fx.internal.cloudapp.net>
2020-05-15 22:18:18 +0000

    fix

--- modules/playbook-execution/main.tf
+++ modules/playbook-execution/main.tf
@@ -3,15 +3,10 @@ resource null_resource "install_docker" {
   provisioner "local-exec" {
     command = <<EOT
     OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES \
-    AZURE_RESOURCE_GROUPS="${var.az_resource_group}" \
     ANSIBLE_HOST_KEY_CHECKING="False" \
     ansible all --inventory '${var.dns_label}', \
     -m ping -c paramiko -u '${var.username}' \
     --extra-vars="{ \"ansible_password\": \"${var.password}\"}"
      EOT
-
-    environment = {
-      HOSTS = "${var.vms_configured}"
-    }
   }
 }
