16ec7e020e746827f4933b817159407424fc6a13
Santiago Ochoa <santy@CPX-TETGOI9G9DL.dir.svc.accenture.com>
2020-05-16 15:33:37 +0200

    add cambios para desplegar docker

--- vm.tf
+++ vm.tf
@@ -85,20 +85,20 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
-# module "configure_vm" {
-#   source                = "./modules/playbook-execution/"
-#   username              = var.username
-#   password              = var.password
-#   dns_label             = "${var.dns_label}.northeurope.cloudapp.azure.com"
-#   ansible_playbook_path = "./ansible/install_docker.yml"
-#   vm_id                 = azurerm_virtual_machine.main.id
-# }
-
-module "configure_docker" {
+module "configure_vm" {
   source                = "./modules/playbook-execution/"
   username              = var.username
   password              = var.password
   dns_label             = "${var.dns_label}.northeurope.cloudapp.azure.com"
-  ansible_playbook_path = "./ansible/deploy_blog.yml"
+  ansible_playbook_path = "./ansible/install_docker.yml"
   vm_id                 = azurerm_virtual_machine.main.id
 }
+
+# module "configure_docker" {
+#   source                = "./modules/playbook-execution/"
+#   username              = var.username
+#   password              = var.password
+#   dns_label             = "${var.dns_label}.northeurope.cloudapp.azure.com"
+#   ansible_playbook_path = "./ansible/deploy_blog.yml"
+#   vm_id                 = azurerm_virtual_machine.main.id
+# }
